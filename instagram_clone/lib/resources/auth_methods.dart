1fb9eede07bf7c643d2b66ef05f7aac1285fd0ee
asfrod <asfrod12@gmail.com>
2023-02-07 13:43:24 -0500

    Configurado firebase auth, revisar que pasa cuando no se selecciona imagen de perfil

--- instagram_clone/lib/resources/auth_methods.dart
+++ instagram_clone/lib/resources/auth_methods.dart
@@ -1,10 +1,13 @@
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/resources/storage_methods.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<String> signupUser({
     required String email,
@@ -15,13 +18,30 @@ class AuthMethods {
   }) async {
     String res = 'Some error ocurred';
     try {
-      if (email.isEmpty ||
-          password.isEmpty ||
-          username.isEmpty ||
-          bio.isEmpty ||
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty ||
           file != null) {
+        //register user
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
+
+        String photoUrl = await StorageMethods()
+            .uploadImageToStorage('profilePics', file, false);
+
+        //add user to our database
+        await _firestore.collection('users').doc(cred.user!.uid).set({
+          'username': username,
+          'uid': cred.user!.uid,
+          'email': email,
+          'bio': bio,
+          'followers': [],
+          'following': [],
+          'photoURL': photoUrl,
+        });
+
+        res = 'success';
       }
     } catch (e) {
       res = e.toString();
--- instagram_clone/lib/screens/signup_screen.dart
+++ instagram_clone/lib/screens/signup_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/resources/auth_methods.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/utils.dart';
 import 'package:instagram_clone/widgets/text_field_input.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -15,6 +20,8 @@ class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
+  Uint8List? _image;
+  bool _isLoading = false;
 
   @override
   void dispose() {
@@ -23,6 +30,34 @@ class _SignupScreenState extends State<SignupScreen> {
     _passwordController.dispose();
     _bioController.dispose();
     _usernameController.dispose();
+  }
+
+  void selectImage() async {
+    Uint8List im = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = im;
+    });
+  }
+
+  void signUpUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String res = await AuthMethods().signupUser(
+        email: _emailController.text,
+        password: _passwordController.text,
+        username: _usernameController.text,
+        bio: _bioController.text,
+        file: _image!);
+    print(res);
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (res != 'success') {
+      showSnackBar(res, context);
+    }
   }
 
   @override
@@ -40,7 +75,7 @@ class _SignupScreenState extends State<SignupScreen> {
               child: Container(),
             ),
             SvgPicture.asset(
-              'ic_instagram.svg',
+              'assets/ic_instagram.svg',
               color: kPrimaryColor,
               height: 64,
             ),
@@ -49,18 +84,19 @@ class _SignupScreenState extends State<SignupScreen> {
             ),
             Stack(
               children: [
-                const CircleAvatar(
-                  radius: 64,
-                  backgroundImage: NetworkImage(
-                      'https://images.unsplash.com/photo-1675685828125-1f6bee17eefa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
-                ),
+                _image != null
+                    ? CircleAvatar(
+                        radius: 64, backgroundImage: MemoryImage(_image!))
+                    : const CircleAvatar(
+                        radius: 64,
+                        backgroundImage: NetworkImage(
+                            'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg'),
+                      ),
                 Positioned(
                   bottom: -10,
                   left: 80,
                   child: IconButton(
-                    onPressed: () {
-                      //TODO: on tap
-                    },
+                    onPressed: selectImage,
                     icon: const Icon(Icons.add_a_photo),
                   ),
                 )
@@ -102,9 +138,7 @@ class _SignupScreenState extends State<SignupScreen> {
             SizedBox(
               width: double.infinity,
               child: TextButton(
-                onPressed: () {
-                  //TODO: on tap
-                },
+                onPressed: signUpUser,
                 style: ButtonStyle(
                   alignment: Alignment.center,
                   padding: const MaterialStatePropertyAll(
@@ -114,7 +148,13 @@ class _SignupScreenState extends State<SignupScreen> {
                   )),
                   backgroundColor: const MaterialStatePropertyAll(kBlueColor),
                 ),
-                child: const Text('Log In'),
+                child: _isLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(
+                          color: kPrimaryColor,
+                        ),
+                      )
+                    : const Text('Sign Up'),
               ),
             ),
             const SizedBox(
@@ -129,7 +169,7 @@ class _SignupScreenState extends State<SignupScreen> {
               children: [
                 Container(
                   padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: const Text("Don't have an account?"),
+                  child: const Text("Already have an account?"),
                 ),
                 GestureDetector(
                   onTap: () {
@@ -138,7 +178,7 @@ class _SignupScreenState extends State<SignupScreen> {
                   child: Container(
                     padding: const EdgeInsets.symmetric(vertical: 8),
                     child: const Text(
-                      "Sign up.",
+                      "Login.",
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ),
